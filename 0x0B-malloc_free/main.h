45989328057e6dbf9c678574c256075b68679795
rashmany <rasheedishola62@gmail.com>
2022-10-03 09:49:21 -0700

    A function that print array of integer

--- 0x0B-malloc_free/main.h
+++ 0x0B-malloc_free/main.h
@@ -2,9 +2,7 @@ fndef MAIN_H
 
 #define MAIN_H
 
-#include <stdio.h>
 
-#include <stdlib.h>
 
 int _putchar(char c);
 
@@ -19,5 +17,7 @@ int **alloc_grid(int width, int height);
 void free_grid(int **grid, int height);
 
 char *argstostr(int ac, char **av);
+
+
 
 #endif
